1aa7145c4689bb642441afcd1dffd8d3707c943b
Andre Leitzke <355407+leitzke-andre@users.noreply.github.com>
2024-06-16 10:46:57 +0200

    Added first models and registered to admin

--- petcheck/settings.py
+++ petcheck/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "home.apps.HomeConfig",
+    "pets.apps.PetsConfig"
 ]
 
 MIDDLEWARE = [
@@ -106,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,31 @@
+from enum import Enum
+
 from django.db import models
 
+
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=50)
+    race = models.IntegerField
+    breed = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    sex = models.IntegerField
+    is_castrated = models.BooleanField()
+
+
+class PetWeight(models.Model):
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
+    weight = models.IntegerField()
+    date = models.DateField()
+
+
+class Race(Enum):
+    CAT = 1
+    DOG = 2
+    BIRD = 3
+
+
+class Sex(Enum):
+    MALE = 1
+    FEMALE = 2
+    OTHER = 3
